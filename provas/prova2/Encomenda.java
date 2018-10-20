9ae1e6f4e7a6c992cb1a8a74d630533af3976aa7
Eduardo Pereira <eduardo.pereira.santos@ccc.ufcg.edu.br>
2018-10-20 00:41:06 -0300

    adicionado o equals

--- provas/prova2/Encomenda.java
+++ provas/prova2/Encomenda.java
@@ -7,6 +7,28 @@ public abstract class Encomenda {
 		this.id = id;
 	}
 
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + id;
+		return result;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Encomenda other = (Encomenda) obj;
+		if (id != other.id)
+			return false;
+		return true;
+	}
+
 	public abstract String toString();
 
 	public abstract int retornaCusto();
